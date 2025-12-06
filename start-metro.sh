708536179c3b3902bad4f31161799cc1222b6051
EMIL <EMANCERO@VARSITHUB.APP>
2025-12-05 23:16:54 -0500

    chore: Xcode config updates and Metro startup scripts

---
+++ start-metro.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /Users/varsityhub/Desktop/CODE/VarsityHubMobile
+export PATH=/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+exec node_modules/.bin/expo start --dev-client
