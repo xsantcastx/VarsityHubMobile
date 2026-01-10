b52e32bd8517e4a5f4155acc080e521ee7ea982c
xsantcastx <xsantcastx@gmail.com>
2026-01-10 20:22:00 +0100

    chore: update migration cleanup and package.json formatting

--- server/prisma/migrations/20260110184500_clean_failed_migration/migration.sql
+++ server/prisma/migrations/20260110184500_clean_failed_migration/migration.sql
@@ -3,6 +3,6 @@
 
 -- Remove the failed migration entry from the migrations table
 DELETE FROM "_prisma_migrations" 
-WHERE migration_name = 'add_severity_to_reports';
+WHERE migration_name = '20260110183000_add_severity_to_reports';
 
 -- This migration does nothing else - it just cleans up the failed state
